ae2758be28aeef12c42b1ba109e07308c07f4819
Daniel Oiteiro <danieloiteiro@gmail.com>
2023-09-27 12:17:25 +0200

    Plausible

--- src/app/components/contactform.js
+++ src/app/components/contactform.js
@@ -8,7 +8,6 @@ function ContactForm() {
     message: "",
   });
 
-  const [isAnimationVisible, setIsAnimationVisible] = useState(false);
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -34,8 +33,7 @@ function ContactForm() {
       });
 
       if (response.ok) {
-        // If the email is sent successfully, show the success message and animation
-        setIsAnimationVisible(true);
+        // If the email is sent successfully, show the success message
         setSuccessMessage(
           "Thanks for reaching out! I will get back to you as soon as possible."
         );
@@ -47,11 +45,10 @@ function ContactForm() {
           message: "",
         });
 
-        // Hide the animation and success message after 5 seconds
+        // Hide the success message after 5 seconds
         setTimeout(() => {
-          setIsAnimationVisible(false);
           setSuccessMessage("");
-        }, 500000);
+        }, 100000);
       } else {
         // Handle the case where sending the email failed
         setErrorMessage(
@@ -60,7 +57,6 @@ function ContactForm() {
       }
     } catch (error) {
       // Handle any network or other errors here
-      console.error("An error occurred:", error);
       setErrorMessage(
         "Oops! Something went wrong, please try submitting your message again."
       );
@@ -69,66 +65,80 @@ function ContactForm() {
 
   return (
     <div className="px-20 lg:mx-48 pt-16">
-      {/* Display the animation on top */}
-      {isAnimationVisible && (
-        <div className="mb-2">
-          <iframe
-            style={{ border: "none", width: "350px", height: "350px" }}
-            src="https://rive.app/s/D6VZao6wok2LZqzVqKrTDQ/embed"
-          ></iframe>
-        </div>
-      )}
+      <div className="form-container">
+        {/* Display success message */}
+        {successMessage && (
+          <p className="success-message" style={{ margin: "8rem 0" }}>
+            {successMessage}
+          </p>
+        )}
 
-      {/* Display success message */}
-      {successMessage && <p>{successMessage}</p>}
-
-      {/* Hide the form if the animation is visible */}
-      {!isAnimationVisible && !successMessage && (
-        <form onSubmit={handleSubmit} className="flex flex-col">
-          <div className="flex-col" >
-            <div>
-              <Input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                labelPlacement="inside"
-                placeholder="Your name"
-              />
+        {/* Hide the form if there is a success message */}
+        {!successMessage && (
+          <form onSubmit={handleSubmit} className="flex flex-col">
+            <div className="flex-col">
+              <div>
+                <Input
+                  type="email"
+                  id="email"
+                  name="email"
+                  labelPlacement="inside"
+                  placeholder="Your email *"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="pt-4">
+                <Input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  labelPlacement="inside"
+                  placeholder="Your name"
+                />
+              </div>
             </div>
+
             <div className="pt-4">
-              <Input
-                type="email"
-                id="email"
-                name="email"
-                labelPlacement="inside"
-                placeholder="Your email *"
-                value={formData.email}
+              <Textarea
+                maxRows={5}
+                placeholder="Your message"
+                name="message"
+                value={formData.message}
                 onChange={handleChange}
               />
             </div>
-          </div>
-
-          <div className="pt-4">
-            <Textarea
-              maxRows={5}
-              placeholder="Your message"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="pt-8">
-            <Button variant="flat" color="default" radius="full" type="submit">
-              Send
-            </Button>
-          </div>
-        </form>
-      )}
+            <div className="pt-8">
+              <Button
+                className={`${
+                  formData.email ? "buttonenabled" : "buttondisabled"
+                }`}
+                variant="flat"
+                color="default"
+                radius="full"
+                type="submit"
+              >
+                Send
+              </Button>
+            </div>
+          </form>
+        )}
+      </div>
 
       {/* Display error message below the form */}
       {errorMessage && <p className="mt-4 text-red-500">{errorMessage}</p>}
+
+      <style jsx>{`
+        .form-container {
+          text-align: center;
+        }
+
+        .success-message {
+          margin: 0;
+        }
+      `}</style>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -10,102 +10,107 @@ import Hello from "./components/hello";
 import History from "./components/history";
 import HistoryMobile from "./components/history_mobile";
 import ContactForm from "./components/contactform";
-import Thatscreen from "./components/loadme/thatscreen"
-
-
+import Thatscreen from "./components/loadme/thatscreen";
+import PlausibleProvider from "next-plausible";
 
 export default function Home() {
-  const [loading, setLoading] = React.useState(true)
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
-    setTimeout(() => setLoading(false), 8000)
-  }, [])
+    setTimeout(() => setLoading(false), 8000);
+  }, []);
 
   return (
     <>
-    {!loading ? (
-      <React.Fragment>
-      <NextUIProvider className="bg-white w-full text-black">
-        <div className="min-h-screen">
-          <div className=" px-20 py-10 w-full bg-white  flex-row  justify-between items-center inline-flex">
-            <div className=" mr-2 md:mr-44">
-              <span className=" text-xl  md:text-3xl max-w-5xl lg:max-w-4xl leading-tight">
-                <span style={{ fontWeight: "bold" }}>I am Daniel Oiteiro</span>{" "}
-                and I thrive in crafting seamless experiences that delight users
-                and drive business growth.
-              </span>
-            </div>
-            <div className="basis-1/4"></div>
-          </div>
+      {!loading ? (
+        <React.Fragment>
+          <PlausibleProvider domain="danieloitei.ro">
+            <NextUIProvider className="bg-white w-full text-black">
+              <div className="min-h-screen">
+                <div className=" px-20 py-10 w-full bg-white  flex-row  justify-between items-center inline-flex">
+                  <div className=" mr-2 md:mr-44">
+                    <span className=" text-xl  md:text-3xl max-w-5xl lg:max-w-4xl leading-tight">
+                      <span style={{ fontWeight: "bold" }}>
+                        I am Daniel Oiteiro
+                      </span>{" "}
+                      and I thrive in crafting seamless experiences that delight
+                      users and drive business growth.
+                    </span>
+                  </div>
+                  <div className="basis-1/4"></div>
+                </div>
 
-          <div
-            className="hidden md:block"
-            style={{ width: "100vw", height: "80vh" }}
-          >
-            <Hello />
-          </div>
+                <div
+                  className="hidden md:block"
+                  style={{ width: "100vw", height: "80vh" }}
+                >
+                  <Hello />
+                </div>
 
-          <div className="md:hidden py-36">
-            <video autoPlay loop muted playsInline>
-              <source src="./videos/hello.mp4" type="video/mp4" />
-            </video>
-          </div>
-        </div>
-        <div className="px-20 text-4xl md:text-6xl leading-none">
-          10+ Years of UX Expertise
-        </div>
+                <div className="md:hidden py-36">
+                  <video autoPlay loop muted playsInline>
+                    <source src="./videos/hello.mp4" type="video/mp4" />
+                  </video>
+                </div>
+              </div>
+              <div className="px-20 text-4xl md:text-6xl leading-none">
+                10+ Years of UX Expertise
+              </div>
 
-        <div className="hidden md:block py-5" style={{}}>
-          {" "}
-          <History />
-        </div>
+              <div className="hidden md:block py-5" style={{}}>
+                {" "}
+                <History />
+              </div>
 
-        <div className="md:hidden py-5" style={{}}>
-          {" "}
-          <HistoryMobile />
-        </div>
+              <div className="md:hidden py-5" style={{}}>
+                {" "}
+                <HistoryMobile />
+              </div>
 
-        <div className="px-20 pt-44 text-4xl md:text-6xl leading-tight">
-          Featured Work
-        </div>
-        <div className="hidden md:block min-h-screen scroll-smooth">
-          <Works />
-        </div>
+              <div className="px-20 pt-44 text-4xl md:text-6xl leading-tight">
+                Featured Work
+              </div>
+              <div className="hidden md:block min-h-screen scroll-smooth">
+                <Works />
+              </div>
 
-        <div className="md:hidden -mt-16 md:mt-0 min-h-screen scroll-smooth">
-          <WorksMobile />
-        </div>
+              <div className="md:hidden -mt-16 md:mt-0 min-h-screen scroll-smooth">
+                <WorksMobile />
+              </div>
 
-        <div className="px-20 pt-44 text-4xl md:text-6xl leading-tight	">
-          Fusing tools and ideas
-        </div>
-        <div
-          className="hidden md:block"
-          style={{ width: "100vw", height: "100vh" }}
-        >
-          <Balls />
-        </div>
-        <div className="md:hidden">
-          <video autoPlay muted playsInline>
-            <source src="./videos/balls.mp4" type="video/mp4" />
-          </video>
-        </div>
-        <div className="px-20 pt-44 text-4xl md:text-6xl leading-none">
-          <div className="flex flex-col">
-            <div>Have an idea?</div>
-            <div>
-              <span style={{ fontWeight: "bold" }}>Let’s chat about it</span>
-            </div>
-          </div>
-        </div>
-        <div className="pb-44 md:pb-0">
-          <ContactForm />
-        </div>
-        <div className="md:block hidden max-w-screen md:pb-24">
-          <Spline scene="https://prod.spline.design/3fIun2Ia365Y7UtH/scene.splinecode" />
-        </div>
-      </NextUIProvider>
-      </React.Fragment>
+              <div className="px-20 pt-44 text-4xl md:text-6xl leading-tight	">
+                Fusing tools and ideas
+              </div>
+              <div
+                className="hidden md:block"
+                style={{ width: "100vw", height: "100vh" }}
+              >
+                <Balls />
+              </div>
+              <div className="md:hidden">
+                <video autoPlay muted playsInline>
+                  <source src="./videos/balls.mp4" type="video/mp4" />
+                </video>
+              </div>
+              <div className="px-20 pt-44 text-4xl md:text-6xl leading-none">
+                <div className="flex flex-col">
+                  <div>Have an idea?</div>
+                  <div>
+                    <span style={{ fontWeight: "bold" }}>
+                      Let’s chat about it
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="pb-44 md:pb-0">
+                <ContactForm />
+              </div>
+              <div className="md:block hidden max-w-screen md:pb-24">
+                <Spline scene="https://prod.spline.design/3fIun2Ia365Y7UtH/scene.splinecode" />
+              </div>
+            </NextUIProvider>
+          </PlausibleProvider>
+        </React.Fragment>
       ) : (
         <Thatscreen />
       )}
