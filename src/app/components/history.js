da9c2b5ef9f484c822d40cbe04a43291eef9bbe0
Daniel Oiteiro <danieloiteiro@gmail.com>
2023-10-18 15:39:01 +0200

    all videos

--- src/app/components/history.js
+++ src/app/components/history.js
@@ -1,61 +1,107 @@
-import * as THREE from 'three'
-import { useEffect, useState, useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { RoundedBox, useCursor } from '@react-three/drei'
-import { Effects } from './Effects'
+import * as THREE from "three";
+import { useEffect, useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { RoundedBox, useCursor } from "@react-three/drei";
+import { EffectComposer, SSR } from "@react-three/postprocessing";
 
 const History = () => (
-  <Canvas shadows gl={{ logarithmicDepthBuffer: true, antialias: false, stencil: false, depth: false }} camera={{ position: [250, 225, 250], fov: 15 }}>
-    <color attach="background" args={['#fff']} />
+  <Canvas
+    shadows
+    gl={{
+      logarithmicDepthBuffer: true,
+      antialias: false,
+      stencil: false,
+      depth: false,
+    }}
+    camera={{ position: [50, 25, 50], fov: 15 }}
+  >
+    <color attach="background" args={["#fff"]} />
     <hemisphereLight intensity={0.5} />
     <directionalLight position={[0, 2, 5]} castShadow intensity={1} />
     <group position={[2, -2, 0]}>
       <group position={[0, -0.9, -3]}>
-        <Plane color="black" rotation-x={-Math.PI / 2} position-z={3} scale={[4, 20, 0.2]} />
-
+        <Plane
+          color="black"
+          rotation-x={-Math.PI / 2}
+          position-z={3}
+          scale={[7, 20, 0.2]}
+        />
       </group>
-      <Sphere />
       <Video />
     </group>
-    <Effects />
+    <EffectComposer disableNormalPass>
+      <SSR />
+    </EffectComposer>
   </Canvas>
-)
+);
 
-function Sphere() {
-  const ref = useRef()
-  const [active, setActive] = useState(false)
-  const [zoom, set] = useState(true)
-  useCursor(active)
-  useFrame((state) => {
-    ref.current.position.y = Math.sin(state.clock.getElapsedTime() / 2)
-    state.camera.position.lerp({ x: 50, y: 25, z: zoom ? 50 : -50 }, 0.03)
-    state.camera.lookAt(0, 0, 0)
-  })
-  return (
-    <mesh ref={ref} receiveShadow castShadow onClick={() => set(!zoom)} onPointerOver={() => setActive(true)} onPointerOut={() => setActive(false)}>
-      <sphereGeometry args={[0.8, 64, 64]} />
-      <meshStandardMaterial color={active ? 'hotpink' : 'lightblue'} clearcoat={1} clearcoatRoughness={0} roughness={0} metalness={0.25} />
-    </mesh>
-  )
-}
 
 const Plane = ({ color, ...props }) => (
-  <RoundedBox receiveShadow castShadow smoothness={10} radius={0.015} {...props}>
-    <meshStandardMaterial color={color} envMapIntensity={0.5} roughness={0} metalness={0} />
+  <RoundedBox
+    receiveShadow
+    castShadow
+    smoothness={10}
+    radius={0.015}
+    {...props}
+  >
+    <meshStandardMaterial
+      color={color}
+      envMapIntensity={0.5}
+      roughness={0}
+      metalness={0}
+    />
   </RoundedBox>
-)
+);
 
-function Video() {
-  const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/videos/drei.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }))
-  useEffect(() => void video.play(), [video])
-  return (
-    <mesh position={[-2, 4, 0]} rotation={[0, Math.PI / 2, 0]} scale={[17, 10, 1]}>
-      <planeGeometry />
-      <meshBasicMaterial toneMapped={false}>
-        <videoTexture attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
-      </meshBasicMaterial>
-    </mesh>
-  )
-}
+const videoSources = [
+    "/videos/conversational design_img0.mp4",
+    "/videos/coding_img0.mp4",
+    "/videos/motion design_img0.mp4",
+    "/videos/talks_img0.mp4",
+  ];
+  
+  function Video() {
+    const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+    const [video] = useState(() =>
+      Object.assign(document.createElement("video"), {
+        crossOrigin: "Anonymous",
+        loop: true,
+        muted: true,
+      })
+    );
+  
+    useEffect(() => {
+      const playVideo = () => {
+        video.src = videoSources[currentVideoIndex];
+        video.play();
+        setTimeout(() => {
+          video.pause();
+          setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videoSources.length);
+        }, 5000);
+      };
+  
+      playVideo();
+  
+      // Clean up video element on component unmount
+      return () => {
+        video.pause();
+        video.src = "";
+      };
+    }, [currentVideoIndex, video]);
+  
+    return (
+      <mesh
+        position={[-2, 4, 0]}
+        rotation={[0, Math.PI / 2, 0]}
+        scale={[17, 10, 1]}
+      >
+        <planeGeometry />
+        <meshBasicMaterial toneMapped={false}>
+          <videoTexture attach="map" args={[video]} />
+        </meshBasicMaterial>
+      </mesh>
+    );
+  }
+  
 
 export default History;
