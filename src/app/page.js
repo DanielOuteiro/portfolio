2b457b583bbcc70fd61a5ed3b78faec9d9934c92
Daniel Oiteiro <danieloiteiro@gmail.com>
2023-10-31 17:10:41 +0100

    Updated Cursor

--- src/app/page.js
+++ src/app/page.js
@@ -200,26 +200,26 @@ export default function Home() {
           </div>
         </NextUIProvider>
         <AnimatedCursor
-          innerSize={8}
-          outerSize={8}
-          color="0, 0, 0"
-          outerAlpha={0.2}
-          innerScale={0.7}
-          outerScale={5}
-          clickables={[
-            "a",
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            "label[for]",
-            "select",
-            "textarea",
-            "button",
-            ".link",
-          ]}
-        />
+      innerSize={8}
+      outerSize={8}
+      color='0, 0, 0'
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link'
+      ]}
+    /> 
       </PlausibleProvider>
     </>
   );
