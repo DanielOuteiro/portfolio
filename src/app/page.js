28bb7ef41f5fddcc9b4ca2947730619242bc34f7
Daniel Oiteiro <danieloiteiro@gmail.com>
2023-11-06 12:19:49 +0100

    Removed Cursor

--- src/app/page.js
+++ src/app/page.js
@@ -199,27 +199,6 @@ export default function Home() {
             </Card>
           </div>
         </NextUIProvider>
-        <AnimatedCursor
-      innerSize={8}
-      outerSize={8}
-      color='0, 0, 0'
-      outerAlpha={0.2}
-      innerScale={0.7}
-      outerScale={5}
-      clickables={[
-        'a',
-        'input[type="text"]',
-        'input[type="email"]',
-        'input[type="number"]',
-        'input[type="submit"]',
-        'input[type="image"]',
-        'label[for]',
-        'select',
-        'textarea',
-        'button',
-        '.link'
-      ]}
-    /> 
       </PlausibleProvider>
     </>
   );
